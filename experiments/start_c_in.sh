554d3f260f2f8bb714e09fcc685714a8201df5c4
creos92 <33228553+creos92@users.noreply.github.com>
2018-03-26 16:27:05 +0200

    Update start_c_in.sh

--- experiments/start_c_in.sh
+++ experiments/start_c_in.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./devel/setup.bash
 sed -i "s/indirizzoipserver/"$IP"/g" ../Client/client_in/client.conf
-chmod +rx start_play.sh /Client/client_in/start_client.sh &&
+chmod +rx /Client/client_in/start_client.sh &&
 cd /Client/client_in && sync && ./start_client.sh &
 sleep 3s
 export ROS_IP=$(/sbin/ifconfig | grep "inet addr:10.8" |awk -F: '{print $2}' | awk '{print $1}')
