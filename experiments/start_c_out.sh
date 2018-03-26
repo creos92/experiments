434682a8aa0c1fb15083c296281354dc6e9cdfaf
creos92 <33228553+creos92@users.noreply.github.com>
2018-03-26 17:03:15 +0200

    Update start_c_out.sh

--- experiments/start_c_out.sh
+++ experiments/start_c_out.sh
@@ -3,10 +3,8 @@ source ./devel/setup.bash
 sed -i "s/indirizzoipserver/"$IP"/g" ../Client/client_out/client.conf
 chmod +rx /Client/client_out/start_client.sh &&
 cd /Client/client_out && sync && ./start_client.sh &
-sleep 3s
+sleep 6s
 export ROS_IP=$(/sbin/ifconfig | grep "inet addr:10.8" |awk -F: '{print $2}' | awk '{print $1}')
 export ROS_MASTER_URI=http://10.8.0.1:11311
 ntpdate ntp.ubuntu.com && 
 cd /experiments && source ./devel/setup.bash && python sub.py && cp data.dat ../data && cat data.dat
-
-
