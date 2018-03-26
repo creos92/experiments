9314f4dd4e01045cd05028cd035c88d87a186fc9
creos92 <33228553+creos92@users.noreply.github.com>
2018-03-26 19:26:07 +0200

    Update start_c_in.sh

--- experiments/start_c_in.sh
+++ experiments/start_c_in.sh
@@ -3,7 +3,7 @@ source ./devel/setup.bash
 sed -i "s/indirizzoipserver/"$IP"/g" ../Client/client_in/client.conf
 chmod +rx /Client/client_in/start_client.sh &&
 cd /Client/client_in && sync && ./start_client.sh &
-sleep 6ss
+sleep 5s
 export ROS_IP=$(/sbin/ifconfig | grep "inet addr:10.8" |awk -F: '{print $2}' | awk '{print $1}')
 export ROS_MASTER_URI=http://10.8.0.1:11311
 ntpdate ntp.ubuntu.com && 
